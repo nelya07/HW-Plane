1e4a248493ac823eef9b71cc45f68b8f0fe4c9da
nelya07 <75816118+nelya07@users.noreply.github.com>
2020-12-18 03:33:48 +0300

    HW Plane

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody))]
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    [SerializeField] private GameObject _template;
+    [SerializeField] private float _cooldown = 2f;
+    [SerializeField] private float time = 0;
+
+    private void Start()
+    {
+        SpawnElements();
+    }
+
+    private void SpawnElements()
+    {
+       Instantiate(_template, transform.position, Quaternion.identity);
+        
+    }
+    private void Update()
+    {
+        transform.position += Vector3.forward * _speed;
+        time += Time.deltaTime;
+        if (time >= _cooldown)
+        {
+            SpawnElements();
+            time = 0;
+        }
+    }
+}
